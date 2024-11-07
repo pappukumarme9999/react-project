f24601212a0b76c3641f67b789a6b856476d0e53
Pappu Kumar <154418180+pappukumarme9999@users.noreply.github.com>
2024-11-07 20:12:34 +0530

    Update Contact.js

--- booknest-master/src/components/Contact/Contact.js
+++ booknest-master/src/components/Contact/Contact.js
@@ -109,45 +109,7 @@ export default Contact;
   <div className="row">
     <div className="col-lg-12">
       <div id="googleMap">
-        {/* <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3680.0810468603486!2d75.86722411215305!3d22.725228788819347!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3962fd6e64d65a21%3A0x9d1e020df91d27e9!2sInfoBeans%20Foundation!5e0!3m2!1sen!2suk!4v1683635713909!5m2!1sen!2suk"
-          width="100%"
-          height="100%"
-          style={{ border: "0" }}
-          allowfullscreen=""
-          loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade"
-        ></iframe> */}
-
-        {/* <iframe
-          src="https://www.google.com/maps/embed/v1/view?key=AIzaSyBMlLa3XrAmtemtf97Z2YpXwPLlimRK7Pk&center=18.514041540658845,73.82060788958046&zoom=15"
-          width="100%"
-          height="100%"
-          style={{ border: 0 }}
-          allowFullScreen
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-        ></iframe> */}
-
-        {/* <iframe
-          src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBMlLa3XrAmtemtf97Z2YpXwPLlimRK7Pk&placeid=ChIJb6N7l5e_wjsRQyDLdpvhqBc"
-          width="100%"
-          height="100%"
-          style="border:0;"
-          allowfullscreen=""
-          loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade"
-        ></iframe> */}
-
-        {/* <iframe
-          src="https://www.google.com/maps/embed/v1/view?key=AIzaSyBMlLa3XrAmtemtf97Z2YpXwPLlimRK7Pk&center=18.514041540658845,73.82060788958046&zoom=15"
-          width="100%"
-          height="100%"
-          style="border:0;"
-          allowfullscreen=""
-          loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade"
-        ></iframe> */}
+        
 
       </div>
     </div>
